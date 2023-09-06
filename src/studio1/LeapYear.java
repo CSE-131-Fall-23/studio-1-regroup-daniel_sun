12c38e333072ea02e5c57f8132cf642f5806ec5d
shlt <shlt@ginnys-air-2.lan>
2023-09-06 15:37:06 -0500

    1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+import java.util.Scanner;
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("the year you want to check:");
+		int y = in.nextInt();
+		boolean leapyear = ((y%4==0)&&(y%100==0))||(y%400==0);
+		System.out.println(y+" is a leap year: "+leapyear);
+		
+
+	}
+
+}
